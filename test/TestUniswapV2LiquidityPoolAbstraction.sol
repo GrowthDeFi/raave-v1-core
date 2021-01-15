73755f1e2e8b2f897e3a8081be1f9be777bb599a
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2021-01-15 18:23:13 -0300

    Adds the basic skeleton for testing.

---
+++ test/TestUniswapV2LiquidityPoolAbstraction.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.6.0;
+
+import { Assert } from "truffle/Assert.sol";
+
+import { Env } from "./Env.sol";
+
+import { UniswapV2LiquidityPoolAbstraction } from "../contracts/modules/UniswapV2LiquidityPoolAbstraction.sol";
+
+contract TestUniswapV2LiquidityPoolAbstraction is Env
+{
+	function test01() external
+	{
+	}
+}
